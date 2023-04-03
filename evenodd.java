7ce435d4079d4c366e7b624c3fcd9614885755f4
SDE-JYOTHIR <117816003+SDE-JYOTHIR@users.noreply.github.com>
2023-04-03 15:13:55 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class evenodd
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter  the no:");
+int a=Sc.nextInt();
+if(a%2==0)
+{
+System.out.println(a+"is even no");
+}
+else
+{
+System.out.println(a+"is odd no");
+}
+}
+}
+
