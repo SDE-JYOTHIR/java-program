60ffaa97660f5ee2e7a7c19929cacfad9ce4c848
SDE-JYOTHIR <117816003+SDE-JYOTHIR@users.noreply.github.com>
2023-07-03 13:52:42 +0530

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,8 @@
+mca@ccn-Vostro-3470:~$ java Abstract
+Enter the radius of the circle :2
+Area of circle is :12.56
+Enter the side of the square :4
+Area of square is :16.0
+Enter the length of the rectangle :5
+Enter the breadth of the rectangle :6
+Area of rectangle is :30.0
